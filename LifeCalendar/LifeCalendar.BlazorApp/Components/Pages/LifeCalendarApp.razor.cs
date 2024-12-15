995f0c8c29e4d2e5e3a038ba8d11f86765be3eb3
Duker <m.oliver.henriksson@gmail.com>
2024-12-15 22:24:52 +0100

    First pass structuring project

---
+++ LifeCalendar/LifeCalendar.BlazorApp/Components/Pages/LifeCalendarApp.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+
+namespace LifeCalendar.BlazorApp.Components.Pages;
+
+public partial class LifeCalendarApp : IAsyncDisposable {
+    private byte[] imgBytes = [];
+    [Inject] IJSRuntime JS { get; set; } = null!;
+    private IJSObjectReference? JSFuncs;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender) {
+        if (firstRender) {
+            JSFuncs = await JS.InvokeAsync<IJSObjectReference>("import",
+                "./Components/Pages/LifeCalendarApp.razor.js");
+        }
+    }
+
+    private async Task DownloadToFile() {
+        if (JSFuncs != null) {
+            await JSFuncs!.InvokeVoidAsync(
+            "downloadFileFromStream",
+                $"Image.png",
+                new DotNetStreamReference(new MemoryStream(imgBytes))
+            );
+        }
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync() {
+        if (JSFuncs != null) {
+            try {
+                await JSFuncs.DisposeAsync();
+                GC.SuppressFinalize(this);
+            } catch (JSDisconnectedException) { }
+        }
+    }
+}
---
+++ LifeCalendar/LifeCalendar.BlazorApp/Components/Pages/LifeCalendarApp.razor.js
@@ -0,0 +1,18 @@
+﻿export function displayBase64Image(container, base64Image) {
+    const img = document.createElement('img');
+    img.src = base64Image;
+    container.innerHTML = "";
+    container.appendChild(img);
+}
+
+export async function downloadFileFromStream(fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
