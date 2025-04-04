dcdde60ccfeb7320b8f2c6154044178a4f8465ee
Duker <m.oliver.henriksson@gmail.com>
2025-04-04 10:36:08 +0200

    -

--- LifeCalendar/LifeCalendar.Tests/Components/Pages/HomePageTests.cs
+++ LifeCalendar/LifeCalendar.Tests/Components/Pages/HomePageTests.cs
@@ -13,8 +13,8 @@ public class HomePageTests : TestContext
         // Act & Assert: Kontrollera om rubriken finns och innehållet är korrekt
         component.MarkupMatches(@"
             <div class=""container"">
-                <h1>Welcome to Your Life Calendar</h1>
-                <div class=""flexbox"">
+                <h2>Welcome to Your Life Calendar</h2>
+                <div class=""flesxbox"">
                     <div class=""textContainer"">
                         <p>Time flies—but what if you could see your whole life at a glance? A Life Calendar breaks down your years into weeks, giving you a visual way to track progress, set goals, and reflect on the big picture.</p>
                         <p>Whether you're planning for the future, appreciating the present, or gaining perspective on how you spend your time, our Life Calendar app makes it easy to create and customize your own. Start mapping your life today!</p>
@@ -29,6 +29,4 @@ public class HomePageTests : TestContext
             </div>
         ");
     }
-
-    
 }
