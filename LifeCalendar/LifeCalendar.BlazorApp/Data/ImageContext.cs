8de8cfda2e4d03e0a814cbd1d4e6dc46a6f567d7
Johan Olofsson <jool@me.com>
2025-04-04 10:31:02 +0200

    Refactor ImageContext to streamline constructor usage
    Simplified the ImageContext constructor by removing redundant parameters and the configuration dependency. Eliminated the OnConfiguring method in favor of relying solely on DbContextOptions for configuration. This enhances code clarity and aligns with best practices for DbContext usage.

--- LifeCalendar/LifeCalendar.BlazorApp/Data/ImageContext.cs
+++ LifeCalendar/LifeCalendar.BlazorApp/Data/ImageContext.cs
@@ -2,28 +2,8 @@
 
 namespace LifeCalendar.BlazorApp.Data;
 
-public class ImageContext : DbContext
+public class ImageContext(DbContextOptions<ImageContext> options) : DbContext(options)
 {
-    private readonly IConfiguration _configuration;
-
-    public ImageContext(DbContextOptions<ImageContext> options, IConfiguration configuration) : base(options)
-    {
-        _configuration = configuration;
-    }
-    
-    // johan kod
-    public ImageContext(DbContextOptions<ImageContext> options) : base(options)
-    {
-    }
-
-
     public DbSet<ImageEntity>? Images { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (_configuration != null)
-        {
-            optionsBuilder.UseSqlite(_configuration.GetConnectionString("ImageDb"));
-        }
-    }
 }
