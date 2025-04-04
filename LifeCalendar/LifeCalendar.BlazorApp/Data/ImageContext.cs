7f0a94229a98e622dc06abac779699ab364fcc43
Johan Olofsson <jool@me.com>
2025-04-04 10:31:14 +0200

    Remove unnecessary blank line in ImageContext.cs
    This change eliminates an extra blank line at the end of the file to maintain code consistency and adhere to styling guidelines. No functionality is affected by this modification.

--- LifeCalendar/LifeCalendar.BlazorApp/Data/ImageContext.cs
+++ LifeCalendar/LifeCalendar.BlazorApp/Data/ImageContext.cs
@@ -5,5 +5,4 @@ namespace LifeCalendar.BlazorApp.Data;
 public class ImageContext(DbContextOptions<ImageContext> options) : DbContext(options)
 {
     public DbSet<ImageEntity>? Images { get; set; }
-
 }
